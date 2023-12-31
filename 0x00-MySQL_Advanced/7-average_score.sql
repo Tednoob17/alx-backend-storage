932db1568e7c66d21d1b5b6905abbd5047994fa4
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 16:18:18 +0100

    07 beg

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,6 +1,10 @@
 -- Creates a stored procedure ComputeAverageScoreForUser that computes the avg score
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$;
+CREATE PROCEDURE ComputeAverageScoreForUser(
+	IN user_id INT
+)
+BEGIN
 
 END;$$
 DELIMITER ;
