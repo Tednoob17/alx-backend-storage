84ce535efbf5ae141544702f2c778d5c3ce2f54b
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 16:00:47 +0100

    metal bands

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,3 @@
 -- Lists all bands with Glam Rock
-SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan FROM 
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan FROM metal_bands
+WHERE style LIKE '%Glam rock%';
