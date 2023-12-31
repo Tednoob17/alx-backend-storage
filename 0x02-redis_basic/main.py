9acafec482b97bdb74640d1a76699d8f33d93c2c
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 16:49:03 +0100

    cashe callig

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -6,3 +6,14 @@ import redis
 
 Cache = __import__('exercise').Cache
 
+# cache = Cache()
+
+# data = b"hello"
+# key = cache.store(data)
+# print(key)
+
+# local_redis = redis.Redis()
+# print(local_redis.get(key))
+
+cache = Cache()
+
