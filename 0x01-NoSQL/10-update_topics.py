651ba9ebf2c6af0c3e910363eaf243e44bff67e6
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 16:27:06 +0100

    pymongo ?

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -2,14 +2,3 @@
 """
 Update topics
 """
-import pymongo
-
-
-def update_topics(mongo_collection, name, topics):
-    """
-    Update topics
-    """
-    return mongo_collection.update_many(
-        {"name": name},
-        {"$set": {"topics": topics}}
-    )
