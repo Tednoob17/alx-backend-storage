3e56b7b3f33808cbad05a548f7c0d096da84320a
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 16:16:36 +0100

    06 begin

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,6 +1,9 @@
 -- Creates a stored procedure AddBonus that adds a new correction
 DELIMITER $$;
 CREATE PROCEDURE AddBonus(
-
+	IN user_id INTEGER,
+	IN project_name VARCHAR(255),
+	IN score INTEGER
+)
 END;$$
 DELIMITER;
