39e8c7012c68bb82aad7e3b233db7ff53451c4a3
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 16:49:22 +0100

    cache store

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -17,3 +17,19 @@ Cache = __import__('exercise').Cache
 
 cache = Cache()
 
+# TEST_CASES = {
+#     b"foo": None,
+#     123: int,
+#     "bar": lambda d: d.decode("utf-8")
+# }
+
+# for value, fn in TEST_CASES.items():
+#     key = cache.store(value)
+#     assert cache.get(key, fn=fn) == value
+
+# inputs = cache._redis.lrange("{}:inputs".format(cache.store.__qualname__), 0, -1)
+# outputs = cache._redis.lrange("{}:outputs".format(cache.store.__qualname__), 0, -1)
+
+# print("inputs: {}".format(inputs))
+# print("outputs: {}".format(outputs))
+
