4fe6cc134f32d81d598b9595cdbf6b37bd3d9e18
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 16:17:00 +0100

    06 end

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -9,6 +9,7 @@ BEGIN
 	IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
 		INSERT INTO projects (name) VALUES (project_name);
 	END IF;
-
+	INSERT INTO corrections (user_id, project_id, score)
+	VALUES (user_id, (SELECT id from projects WHERE name=project_name), score);
 END;$$
 DELIMITER;
