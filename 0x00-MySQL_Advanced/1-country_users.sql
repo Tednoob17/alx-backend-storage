7748d2192259c5c1f7e8e0da0f33934e71a9175c
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 15:59:51 +0100

    id, name, email

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,8 @@
 -- Create a table users with unique users and country
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
     country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
