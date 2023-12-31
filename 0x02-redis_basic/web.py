729fc3ae52910b57597ec9352f37fb5251aea496
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 16:52:14 +0100

    return

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,3 +6,14 @@ import redis
 import requests
 
 
+store = redis.Redis()
+count = 0
+
+
+def get_page(url: str) -> str:
+    """ get a page and cach value"""
+    store.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    store.incr(f"count:{url}")
+    store.setex(f"cached:{url}", 10, store.get(f"cached:{url}"))
+    return resp.text
