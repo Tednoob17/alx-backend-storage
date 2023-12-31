f2f2af1ad8b5c34c5aa08f597bdd927a5b2f203f
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 16:31:56 +0100

    12 define mongo paths

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -8,4 +8,12 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_logs = client.logs.nginx
+    docs = nginx_logs.count_documents({})
+    get = nginx_logs.count_documents({'method': 'GET'})
+    post = nginx_logs.count_documents({'method': 'POST'})
+    put = nginx_logs.count_documents({'method': 'PUT'})
+    patch = nginx_logs.count_documents({'method': 'PATCH'})
+    delete = nginx_logs.count_documents({'method': 'DELETE'})
+    get_status = nginx_logs.count_documents({'method': 'GET',
+                                             'path': '/status'})
 
