f4b7ec0074676226ead691d26b50c001605c960b
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 16:40:53 +0100

    9 insert school

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,9 +4,3 @@ Function that insert documents
 """
 import pymongo
 
-
-def insert_school(mongo_collection, **kwargs):
-    """
-    Insert a school
-    """
-    return mongo_collection.insert_one(kwargs).inserted_id
