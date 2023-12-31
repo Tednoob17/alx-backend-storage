ab93f85663b30e2c47f457e8cf250010ee6f884a
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 16:31:22 +0100

    12 mongo cli

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,23 +5,3 @@ Get logs stats from mongo
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_logs = client.logs.nginx
-    docs = nginx_logs.count_documents({})
-    get = nginx_logs.count_documents({'method': 'GET'})
-    post = nginx_logs.count_documents({'method': 'POST'})
-    put = nginx_logs.count_documents({'method': 'PUT'})
-    patch = nginx_logs.count_documents({'method': 'PATCH'})
-    delete = nginx_logs.count_documents({'method': 'DELETE'})
-    get_status = nginx_logs.count_documents({'method': 'GET',
-                                             'path': '/status'})
-
-    print("{} logs".format(docs))
-    print("Methods:")
-    print("\tmethod GET: {}".format(get))
-    print("\tmethod POST: {}".format(post))
-    print("\tmethod PUT: {}".format(put))
-    print("\tmethod PATCH: {}".format(patch))
-    print("\tmethod DELETE: {}".format(delete))
-    print("{} status check".format(get_status))
