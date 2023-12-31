597ccb4a2179198d1896be3bc23d549a3b3a978c
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 16:31:49 +0100

    12 define mongo place

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,3 +5,7 @@ Get logs stats from mongo
 from pymongo import MongoClient
 
 
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_logs = client.logs.nginx
+
