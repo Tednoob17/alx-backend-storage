e2f2f5b0daf444c13919131c55da9549cc83bb1e
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 16:51:52 +0100

    cash tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,18 +2,4 @@
 """
 Web Cache and Tracker
 """
-import redis
-import requests
-
-
-store = redis.Redis()
-count = 0
-
-
-def get_page(url: str) -> str:
-    """ get a page and cach value"""
-    store.set(f"cached:{url}", count)
-    resp = requests.get(url)
-    store.incr(f"count:{url}")
-    store.setex(f"cached:{url}", 10, store.get(f"cached:{url}"))
-    return resp.text
+imp
