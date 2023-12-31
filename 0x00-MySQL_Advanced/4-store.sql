9766d695a6fa2cb124d05cc0aca79270434328a6
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 16:03:40 +0100

    04 tasks

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,3 +1,2 @@
 -- Creates a trigger that decreases quantity of an item
-CREATE TRIGGER decrease_q AFTER INSERT ON orders FOR EACH ROW
-UPDATE items SET quantity = quantity - NEW.number WHERE name=NEW.item_name;
+
