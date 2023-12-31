0ec8ca4f10dab73a8a34e033e07bc391ee98fa75
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 16:39:44 +0100

    8 list doc

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,13 +2,3 @@
 """
 Function that list all documents
 """
-import pymongo
-
-
-def list_all(mongo_collection):
-    """
-    list all collections
-    """
-    if not mongo_collection:
-        return []
-    return list(mongo_collection.find())
